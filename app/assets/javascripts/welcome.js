001d633b23dd82f0085ccb12a301034fbd80a61e
barendan <Barendan@gmail.com>
2016-07-29 15:21:12 -0400

    Popout display created via JQuery

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,27 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+
+$(document).ready( function () {
+
+	// $('.js-show').toggleClass('previewProducts');
+
+	$('.container').click( function () {
+		$('.js-show').toggleClass('previewProducts');
+		$('.js-show').toggleClass('hidden');
+
+
+		// $('.js-show').toggleClass('previewProducts');
+
+	});
+
+
+
+// Flashing Title but CSS is already doing this 
+// 	var flashBaby = $('.title');
+	
+// 	setInterval(function() {
+//    	flashBaby.toggleClass('blinking');
+// 	}, 700);
+})
