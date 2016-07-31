f80330c55d85154cac33cc076a745d4e79cc61f7
barendan <Barendan@gmail.com>
2016-07-30 20:04:53 -0400

    Created an about section

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,6 +3,75 @@
 
 $(document).ready( function () {
 
+	// Welcome Preview Map Events
+	$('.js-previewMap').click( function () {
+		$('.js-previewMap').animate({width:'90%'});
+		$('.js-previewMap').removeClass('cthru');
+
+		if (! $('.js-about').hasClass('cthru') ) {
+			$('.js-about').animate({width:'5%'});
+			$('.js-about').addClass('cthru');
+		}
+		if (! $('.js-preview').hasClass('cthru') ) {
+			$('.js-preview').animate({width:'5%'});
+			$('.js-preview').addClass('cthru');
+		}
+	});
+	// $('.js-closeProduct').click( function (e) {
+	// 	e.stopPropagation();
+	// 	$('.js-product').animate({width:'5%'});
+	// 	$('.js-product').addClass('cthru');
+	// });
+
+
+	// Welcome About Events
+	// $('.js-closeAbout').click( function (e) {
+	// 	e.stopPropagation();
+	// 	$('.js-about').animate({width:'5%'});
+	// 	$('.js-about').addClass('cthru');
+	// });
+	$('.js-about').click( function () {
+		$('.js-about').animate({width:'90%'});
+		$('.js-about').removeClass('cthru');
+
+		if (! $('.js-previewMap').hasClass('cthru') ) {
+			$('.js-previewMap').animate({width:'5%'});
+			$('.js-previewMap').addClass('cthru');
+		}
+		if (! $('.js-preview').hasClass('cthru') ) {
+			$('.js-preview').animate({width:'5%'});
+			$('.js-preview').addClass('cthru');
+		}
+		if ( $('.js-previewMap').hasClass('cthru') ) {
+			$('.js-previewMap').animate({width:'5%'});
+			$('.js-previewMap').addClass('cthru');
+		}
+	});
+
+	//Welcome Preview Product events
+	$('.js-preview').click( function () {
+		$('.js-preview').animate({width:'90%'});
+		$('.js-preview').removeClass('cthru');
+
+		if (! $('.js-previewMap').hasClass('cthru') ) {
+			// $('.js-previewMap').animate({width:'5%'});
+			$('.js-previewMap').addClass('cthru');
+		}
+		if (! $('.js-about').hasClass('cthru') ) {
+			$('.js-about').animate({width:'5%'});
+			$('.js-about').addClass('cthru');
+		}
+		// Moving to Review Panel from Map Panel fix
+		if ( $('.js-previewMap').hasClass('cthru') ) {
+			$('.js-previewMap').animate({width:'90%'});
+			// $('.js-review').addClass('cthru');
+		}
+	});
+
+
+
+// --------------------------------------------------
+
 	// Product Events
 	$('.js-product').click( function () {
 		$('.js-product').animate({width:'90%'});
@@ -74,27 +143,6 @@ $(document).ready( function () {
 	});
 
 
-	// Welcome Preview Product panel
-	$('.js-preview').click( function () {
-		$('.js-preview').animate({width:'95%'});
-		$('.js-preview').removeClass('cthru');
-
-		if (! $('.js-previewMap').hasClass('cthru') ) {
-			$('.js-previewMap').animate({width:'5%'});
-			$('.js-previewMap').addClass('cthru');
-		}
-	});
-	
-	// Welcome Preview Map panel
-	$('.js-previewMap').click( function () {
-		$('.js-previewMap').animate({width:'95%'});
-		$('.js-previewMap').removeClass('cthru');
-
-		if (! $('.js-preview').hasClass('cthru') ) {
-			$('.js-preview').animate({width:'5%'});
-			$('.js-preview').addClass('cthru');
-		}
-	});
 
 
 // Flashing Title but CSS is already doing this 
